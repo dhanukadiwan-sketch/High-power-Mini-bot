acea87414dd9dc04361734d31f7ce1593ce71c87
dhanukadiwan-sketch <dhanukadiwan@gmail.com>
2026-01-05 12:36:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ let code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 
 app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
+app.use('/bot', async (req, res, next) => {
+    res.sendFile(__path + '/bot.js')
 });
 app.use('/', async (req, res, next) => {
     res.sendFile(__path + '/main.html')
@@ -19,11 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star ‼️
-
-
-Server running on http://localhost:` + PORT)
+    console.log(`Server running on http://localhost:` + PORT)
 });
 
 module.exports = app;
